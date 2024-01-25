661fa5e053eb0ac56cd84f076ac136c3c744fa36
Liam <49714254+LiamHecht@users.noreply.github.com>
2024-01-25 15:05:35 +0200

    Delete gui directory

--- gui/static/script.js
+++
@@ -1,8 +0,0 @@
-function toggleSettings() {
-    var colorPickerContainer = document.getElementById("colorPickerContainer");
-    colorPickerContainer.style.display = (colorPickerContainer.style.display === "none") ? "block" : "none";
-}
-
-function changeColor(color) {
-    document.body.style.backgroundColor = color;
-}
